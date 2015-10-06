67fd3a8cc1ef39798044b1c691afc8494373a593
somkiat <somkiat.p@gmail.com>
2015-10-06 10:16:44 +0700

    Initial Commit

---
+++ MobiCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobiCalculator
+//
+//  Created by somkiat puisungnoen on 10/6/2558 BE.
+//  Copyright © 2558 ___UP1___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
